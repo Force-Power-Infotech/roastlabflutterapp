f017d698950e38f8452a07e363836b210e0c3487
Rohit Ghosh <140050497+RohitGhosh07@users.noreply.github.com>
2026-05-25 12:08:42 +0530

    Remove unused screens, models, and widgets to streamline the codebase

--- lib/core/constants/app_constants.dart
+++
@@ -1,9 +0,0 @@
-class AppConstants {
-  static const appName = 'RoastLab';
-  static const supportEmail = 'support@roastlab.app';
-  static const warmGradient = [
-    0xFF241814,
-    0xFF5B3C2C,
-    0xFFCAA07A,
-  ];
-}
